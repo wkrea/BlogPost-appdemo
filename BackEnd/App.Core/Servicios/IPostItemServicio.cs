740dd495cea497e26d0799e28d40ca43dff35f1c
wkrea <william.trigos@hotmail.com>
2020-10-12 11:43:36 -0500

    Mejoras a metodos de backend y refactorización de método GuardarContext

--- BackEnd/App.Core/Servicios/IPostItemServicio.cs
+++ BackEnd/App.Core/Servicios/IPostItemServicio.cs
@@ -1,20 +1,21 @@
 ﻿using App.Core.Dominio;
 using App.Core.Dominio.Errors;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace App.Core.Servicios
 {
     public interface IPostItemServicio
     {
-        Task<IEnumerable<ErrorApp>> CrearPostItem(PostItem postItem);
-        Task<IEnumerable<ErrorApp>> CrearComentario(Comentario comment);
-        Task<bool> GetPostItemById(int id);
-        Task EliminarPostItem(int id);
-        Task<IEnumerable<ErrorApp>> EditarPostItem(PostItem postItem);
-        Task<IEnumerable<Comentario>> GetComentariosByPostItemId(int postId);
         Task<IEnumerable<PostItem>> ListarPostsItems();
+        Task<PostItem> GetPostItemById(int id);
+        Task<IEnumerable<ErrorApp>> CrearPostItem(PostItem postItem);
+        Task<IEnumerable<ErrorApp>> EditarPostItem(PostItem postItem);
+        Task EliminarPostItem(int id);
+
+        #region cruce PostItem-->Comentarios
+        Task<IEnumerable<ErrorApp>> CrearComentario(Comentario comment);
+        Task<IEnumerable<Comentario>> GetComentariosByPostItemId(int postId);
+        #endregion
     }
 }
